b4ca3800e5eccd296bd2cbf662ba1b470cb70dac
Pawan Singh <pawansinghrishu@gmail.com>
2025-11-18 20:26:01 +0530

    delete

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "pawancloud-bkt"
-    key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "pawancloud-dynamo-db-table"
-  }
-}
